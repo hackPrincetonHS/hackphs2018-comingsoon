d49c1a462b9f688c449f020cf136f76383e31826
lincolnmroth <lincoln@lroth.tech>
2018-01-07 11:23:54 -0500

    initial commit

---
+++ js/coming-soon.js
@@ -0,0 +1,11 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    poster: "img/hackphsbg.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
